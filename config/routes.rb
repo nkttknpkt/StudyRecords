a0a6d5388189a03740f091110d7b6df8ebc8c322
ktp <nkttkn666@gmail.com>
2023-03-29 12:46:07 +0000

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'learns#index'
+  
+  resources :learns,except: [:show]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
 end
